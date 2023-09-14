d9d6b3c72a0199d460b04c7f69d57d84622706dd
grha6459 <97565231+grha6459@users.noreply.github.com>
2023-09-14 13:25:00 -0600

    first commit

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,15 @@
+import numpy as np
+#not sure where above line should be
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    with open(file_name) as f:  # with will ensure that it closes
+        #result_list is used to store vaues as they are collected, may convert to an array after it is made
+        result_list = []
+        # line is the string that contains the entire line
+        for line in f:
+            # Strip removes newline and trailing whitespaces
+            # Split separated the string by commas
+            #items is a list data type
+            items = line.strip().split(',')
+            if items[query_column] == query_value:
+                result_list.append(items[result_column])
+    return np.array(result_list)
