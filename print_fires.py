8fb899358d20ecc2b8dbfb64d598b13b4b2fcd99
grha6459 <97565231+grha6459@users.noreply.github.com>
2023-09-26 13:01:41 -0600

    Supplemented comments, no functionality changed.

--- print_fires.py
+++ print_fires.py
@@ -4,22 +4,23 @@ import sys
 
 
 def main():
-
+    # Initializing a parser object, which contains arguments with specified properties
     parser = argparse.ArgumentParser(
-                description='This is the preferred way.',
-                prog='print_fires.py'
-    )
+                description='This is the preferred way to parse command line arguments.',
+                prog='print_fires.py')
+
     parser.add_argument('--country',
                         type=str,
                         help='The name of the country used as query',
                         required=True)
 
+    # Following has default of one, a default is also supplied by get_column, but this prevents None from being passed
+    # to my_utils if this argument isn't included in the command line.
     parser.add_argument('--country_column',  # This is the one argument with a default, default is built into my_utils
                         type=int,
                         help='Number (not the index, first column is one) of column query value resides in',
                         required=False,
                         default=1)
-    # default value to one, this is also handled by the function, but prevents None being passed to function
 
     parser.add_argument('--fires_column',
                         type=int,
@@ -32,20 +33,14 @@ def main():
                         required=True)
 
     args = parser.parse_args()
-    # args.fires_column
-
-    # country='United States of America'
-    #  county_column = 1
-    #  fires_column = 4
-    #  file_name = 'Agrofood_co2_emission.csv'
-    # fires = my_utils.get_column(file_name,country,fires_column,query_column=county_column)
+    # Following uses the arguments returned by the parser for a function call
     fires = my_utils.get_column(args.file_name, args.country, args.fires_column, query_column=args.country_column)
-    if fires:  # if fires is None, this conditional is false
+    if fires:  # if fires is None (as indicates an error), this conditional is false
         print(fires)
-        return
     else:
-        sys.exit(1)
+        sys.exit(1)  # Exit code updated to reflect that an error was encountered
 
 
+# Main function added
 if __name__ == "__main__":
     main()
