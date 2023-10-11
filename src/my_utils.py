a263cc39424c60b2ad0617ee6a41f70d7f9b96e8
grha6459 <97565231+grha6459@users.noreply.github.com>
2023-10-11 07:19:47 -0600

    "Updated for PEP8 compliance, my IDE lied to me and had maximum line length set to 120 characters, not 79. Thanks Pycharm \s."

--- src/my_utils.py
+++ src/my_utils.py
@@ -3,25 +3,31 @@ import numpy as np
 
 def get_column(file_name, query_value, result_column, query_column=1):
     """
-    :param file_name: The name of the file to be read from, in csv format, in current directory
+    :param file_name: The name of the file to be read from, in csv format, in
+    current directory
     :param query_value: The value used to screen results in results_column
-    :param result_column: The number (beginning with one) of the column to be returned, in part
-    :param query_column: The number (beginning with one) of the column to be returned, in part, default value of one
-    :return: a list of the numbers found in the result_column, if non-numerical values are found, will include NaN
-             if issues are with file handeling are encountered, function will  return empty array
+    :param result_column: The number (beginning with one) of the column to be
+    returned, in part
+    :param query_column: The number (beginning with one) of the column to be
+    returned, in part, default value of one
+    :return: a list of the numbers found in the result_column, if non-numerical
+    values are found, will include NaN
+    if issues are with file handeling are encountered, function will
+    return empty array
     """
-    # Following Variable (file) is only used as a test, open() is run again below
+    # Following Variable (file) is only used as a test, open() is run again
     file = None
     try:
         file = open(file_name)
     except FileNotFoundError:
-        print("File not found. Is ", file_name, " the file you were looking for?")
+        print("File not found. Is ", file_name, " what you were looking for?")
         return np.array([])
     except PermissionError:
         print("Permission denied. Is ", file_name, " open in another program?")
         return np.array([])
     with open(file_name) as f:  # with will ensure that it closes
-        # result_list is used to store values as they are collected, may convert to an array after it is
+        # result_list is used to store values as they are collected, may
+        # convert to an array after it is
         # made
         result_list = []
         # line is the string that contains the entire line
@@ -34,15 +40,16 @@ def get_column(file_name, query_value, result_column, query_column=1):
                 try:
                     new_value = int(float(items[result_column - 1]))
                     result_list.append(new_value)  # casting to float, then int
-                except ValueError:  # if items[result_column - 1] can't be cast to a string
+                except ValueError:  # if items[result_column - 1] can't be cast
+                    # to a string
                     result_list.append(float('nan'))
-    return np.array(result_list)  # Has been updated again to return numpy array
+    return np.array(result_list)  # Has been updated again to return numpy arr
 
 
 def get_mean(int_array):
     """
     :param int_array: A 1D numpy array of integers
-    :return: The arithmetic mean of the array of integers, calculated using numpy
+    :return: The arithmetic mean of the array of integers, calculated using np
     Notes:
     If array is empty, or
     """
@@ -57,7 +64,7 @@ def get_mean(int_array):
 def get_median(int_array):
     """
     :param int_array: A 1D numpy array of integers
-    :return: The median of the array of integers, as calculated using numpy library
+    :return: The median of the array of integers, as calculated using numpy
 
     """
     if np.shape(int_array)[0] == 0:
@@ -71,7 +78,8 @@ def get_median(int_array):
 def get_std_dev(int_array):
     """
     :param int_array: A 1D numpy array of integers
-    :return: The standard deviation of the array of integers, as calculated using numpy a
+    :return: The standard deviation of the array of integers, as calculated
+    using numpy a
     """
     if np.shape(int_array)[0] == 0:
         return None
---
+++ testing/functional_testing/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This file has been updated to have only functional runs
+
+
+python ../../src/print_fires.py --country 'United States of America' --fires_column 4 --file_name 'test_data/test_data.csv' --operation 'median'
